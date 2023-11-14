b9f2ed595c20cf2f11e5a57ecc735e5e6d167fd1
atharva kavade <124795831+atharvadk@users.noreply.github.com>
2023-11-14 22:10:33 +0530

    Add files via upload

---
+++ bmi_calcu.py
@@ -0,0 +1,19 @@
+import math
+
+print("This is Body-Mass Index calculator\nPlease enter your height as 5.9 if your height is 5 feet 9 inches\nEnter your weight in KGs")
+height_feet,height_inch = rawinput("Height in feet = "))
+
+weight = int(input("Weight = "))
+
+
+
+
+
+he_in = height * 12
+height_final = he_in + height_in
+height_m = height_final * 0.0254
+
+BMI = weight / height_m ** 2
+BMI_final = int(BMI)
+print(BMI_final)
+
